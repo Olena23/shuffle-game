8bed079d1e4e7e5caa75bc82e4e12910e4793dc9
Olena23 <tajlintajlin@gmail.com>
2017-11-01 10:32:31 +0200

    Bug fixes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 window.onload = function () {
+  //uinitiating initial values
   var openedList = [];
   var guessed = 0;
   var timer = 0;
   var movesNum = document.getElementsByClassName('moves')[0];
   var restart = document.getElementsByClassName('restart')[0];
   var displayedTime = document.getElementById('displayedTime');
+  var starDysplayed = document.getElementById('starDysplayed');
   var popUp = document.getElementsByClassName('b-popup')[0];
   var popUpmoves = document.getElementById('popUpmoves');
   var gameSeconds = document.getElementById('gameSeconds');
@@ -13,25 +15,33 @@ window.onload = function () {
   var numberOfMoves = 0;
   var lis = Array.prototype.slice.call(document.getElementsByClassName('card'));
   var stars = Array.prototype.slice.call(document.getElementsByClassName('fa fa-star'));
+  var starStatistics = 3;
   var box = document.getElementsByClassName("deck")[0];
   box.innerHTML = "";
+  //shuffle and re-add all cards to the page
   lis = shuffle(lis);
   for (let i = 0; i < lis.length; i++){
     lis[i].addEventListener("click",check);
     box.appendChild(lis[i])
   }
 
+//Timer handling
+  var refreshIntervalId = setInterval(function () {timerCounter()}, 1000);
+
   function timerCounter(){
     timer+=1;
     displayedTime.innerHTML = timer;
   }
+  function stopTimer() {
+    clearInterval(refreshIntervalId)
+  }
 
-  setInterval(timerCounter, 1000);
-
+  //Resetting the game
   function replay() {
     openedList = [];
     guessed = 0;
     timer = 0;
+    stopTimer();
     movesNum.innerHTML = "0";
     numberOfMoves = 0;
     var lis2 = Array.prototype.slice.call(document.getElementsByClassName('card'));
@@ -47,6 +57,8 @@ window.onload = function () {
     stars.forEach(function (el) {
       el.className = 'fa fa-star'
     })
+    starStatistics = 3;
+    refreshIntervalId = setInterval(function () {timerCounter()}, 1000);
   }
 
   restart.addEventListener("click", replay);
@@ -61,6 +73,7 @@ window.onload = function () {
     replay()
   });
 
+  //Checking id the guess was right
   function check(){
     if((this.className === 'card match') || (this.className === 'card open show')){
     } else {
@@ -70,6 +83,7 @@ window.onload = function () {
     }
   }
 
+  //Compares two cards in the array
   function isMatch() {
     if (openedList.length === 2) {
       if (openedList[0].firstElementChild.className === openedList[1].firstElementChild.className) {
@@ -95,26 +109,29 @@ window.onload = function () {
     }
   }
 
+  //Checks if ALL cards are guessed
   function isWin() {
     if(guessed===16){
       popUpmoves.innerHTML = movesNum.innerHTML;
       gameSeconds.innerHTML = timer;
+      starDysplayed.innerHTML = starStatistics;
+      stopTimer();
       popUp.style.display = 'block';
     }
   }
 
+  // Controls the number of star displayed
   function starHandler() {
-    console.log(stars)
-    if(numberOfMoves > 40){
-      stars[0].className = ''
-    } else if (numberOfMoves > 30) {
-      stars[1].className = ''
+    if (numberOfMoves > 30) {
+      stars[1].className = '';
+      starStatistics = 1;
     } else if (numberOfMoves > 18) {
-      stars[2].className = ''
+      stars[2].className = '';
+      starStatistics = 2;
     }
   }
 
-
+//Shuffles an array
   function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
